a9ad64e6a5a449437f88991f98adb67f46106d36
Khvan4ik <_email@gmail.com>
2026-04-24 02:05:07 +0900

    premium design + telegram + real encar links

--- Server.js
+++ Server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const app = express() app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
