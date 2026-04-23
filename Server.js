809bd62a82a9d85ee98d2e085679dfb4ea06f0b4
Khvan4ik <_email@gmail.com>
2026-04-24 01:33:00 +0900

    fix homepage route

--- Server.js
+++ Server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
+const app = express() app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
